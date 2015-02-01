c7dbb00d27aaa6b1315ee2956af8ada545641913
Benjamin Friedrich <benjamin.friedrich@zalando.de>
2015-02-01 21:59:57 +0100

    implemented multi session runs + adapted all functionality to config mechanism

--- tardis/cli.py
+++ tardis/cli.py
@@ -12,14 +12,9 @@ import collections
 POSTGRES_DATA_MOUNT = '/var/lib/postgresql/data'
 POSTGRES_USER = 'postgres'
 POSTGRES_PASSWORD= 'postgres'
-
-session_file = '.tardis.session'
-
-docker_image = 'postgres:9.4.0'
-host_data_directory = '/tmp/postgres'
-docker_port = 5432
-
 CONFIG_FILE = './tardis2.yml'
+
+SESSION_FILE = '.{}.tardis.session'
 
 
 @click.group()
@@ -35,13 +30,13 @@ def load_configuration():
         return dict()
 
 
-def dump_to_session_data(data):
-    with open(session_file, 'w') as f:
+def dump_to_session_data(travel_plan, data):
+    with open(SESSION_FILE.format(travel_plan), 'w') as f:
         json.dump(data, f)
  
 
-def load_session_data():
-  with open(session_file, 'r') as f:
+def load_session_data(travel_plan):
+  with open(SESSION_FILE.format(travel_plan), 'r') as f:
         return json.load(f)
 
 
@@ -91,30 +86,36 @@ def configure():
 
 # TODO error handling
 @cli.command()
+@click.option('--travel-plan', help='name of the travel plan configuration')
 @click.option('--config_path', help='path to tardis config file', default='.')
-def run(config_path, recent_checkpoint = None):
+def run(travel_plan, config_path, recent_checkpoint = None):
     """
     Runs your local DB image according to tardis configuration
     """
-    
+
+    config = load_configuration()
+    travel_plan_config = config[travel_plan]
+    docker_image = travel_plan_config['image'] + ':' + travel_plan_config['tag']
+
     client = create_docker_client();
 
-    container = client.create_container(docker_image, environment = { 'POSTGRES_USER': POSTGRES_USER, 
-                                                                      'POSTGRES_PASSWORD': POSTGRES_PASSWORD })
+    container = client.create_container(docker_image, environment = { 'POSTGRES_USER':     travel_plan_config['db_user'], 
+                                                                      'POSTGRES_PASSWORD': travel_plan_config['db_password'] })
     container_id = container.get('Id')
 
-    dump_to_session_data({ 'container_id' :     container_id, 
-                           'recent_checkpoint': recent_checkpoint })
+    dump_to_session_data(travel_plan,
+                          { 'container_id' :     container_id, 
+                            'recent_checkpoint': recent_checkpoint })
 
     response = client.start(container = container_id, 
                             binds = { 
-                                        host_data_directory:
+                                        travel_plan_config['data_share']:
                                         {
                                             'bind': POSTGRES_DATA_MOUNT,
                                             'ro': False
                                         }
                                     },
-                            port_bindings = { 5432: docker_port } )
+                            port_bindings = { 5432: travel_plan_config['db_port'] } )
 
     ok('started container "{}"'.format(container_id))
 
@@ -123,46 +124,52 @@ def is_git_directory(dir):
     return git.repo.fun.is_git_dir(dir)
 
 
-def is_dirty():
+def is_dirty(data_share):
     """
     Checks if the current reposity has untracked or changed files
     """
-    repo = git.repo.base.Repo(path=host_data_directory)
+    repo = git.repo.base.Repo(path=data_share)
     return repo.is_dirty(untracked_files=True)
 
 
-def init_git_repo_if_not_exists():
-    if is_git_directory(host_data_directory):
-        click.echo('"{}" is already a GIT repo --> utilizing this repo'.format(host_data_directory))
+def init_git_repo_if_not_exists(path):
+    if is_git_directory(path):
+        click.echo('"{}" is already a GIT repo --> utilizing this repo'.format(path))
     else:
-        git.repo.base.Repo.init(path=host_data_directory)
-        ok('initialized GIT repo in "{}"'.format(host_data_directory))
+        git.repo.base.Repo.init(path=path)
+        ok('initialized GIT repo in "{}"'.format(path))
 
 
 @cli.command()
+@click.option('--travel-plan', help='name of the travel plan configuration')
 @click.option('--checkpoint', help='name of the checkpoint representing the current DB state')
-def save(checkpoint):
+def save(travel_plan, checkpoint):
     """
     Sets a checkpoint for the current DB state
     """
 
-    init_git_repo_if_not_exists()
+    config = load_configuration()
+    data_share = config[travel_plan]['data_share']
 
-    if is_dirty():
+
+    init_git_repo_if_not_exists(data_share)
+
+    if is_dirty(data_share):
         docker_client = create_docker_client()
-        session_data = load_session_data()
+        session_data = load_session_data(travel_plan)
         container_id = session_data['container_id']
 
         try:
             docker_client.pause(container_id)
             ok('paused container "{}"'.format(container_id))
             click.echo('repo has changed...')
-            git_cmd = git.Git(host_data_directory)
-            git_cmd.add('--all', host_data_directory)
+            git_cmd = git.Git(data_share)
+            git_cmd.add('--all', data_share)
             git_cmd.commit(message=checkpoint)
             git_cmd.tag('--annotate', checkpoint, message=checkpoint)
 
-            dump_to_session_data({ 'container_id':      container_id, 
+            dump_to_session_data(travel_plan,
+                                 { 'container_id':      container_id, 
                                    'recent_checkpoint': checkpoint })
         except Exception as e:
             error(e)
@@ -174,47 +181,61 @@ def save(checkpoint):
 
 
 @cli.command('travel-to')
+@click.option('--travel-plan', help='name of the travel plan configuration')
 @click.option('--checkpoint', help='name of the checkpoint representing the DB state you want to switch to')
 @click.pass_context
-def travel_to(ctx, checkpoint):
+def travel_to(ctx, travel_plan, checkpoint):
     """
     Sets DB state back to state saved in the target checkpoint 
     """
-    docker_client = create_docker_client();
-    session_data = load_session_data()
-    container_id = session_data['container_id']
+    config = load_configuration()
+    data_share = config[travel_plan]['data_share']
 
-    docker_client.stop(container_id)
-    ok('stopped container "{}"'.format(container_id))
+    ctx.invoke(stop, travel_plan=travel_plan)
 
-    git_cmd = git.Git(host_data_directory)
+    git_cmd = git.Git(data_share)
     git_cmd.checkout('--force','tags/{}'.format(checkpoint))
     ok('travelled back to "{}"'.format(checkpoint))
     
     # FIXME we need to reuse the same config path as we did in 'travis run'
-    ctx.invoke(run, recent_checkpoint=checkpoint)
+    ctx.invoke(run, travel_plan=travel_plan, recent_checkpoint=checkpoint)
 
 
 
 
 @cli.command('travel-back')
+@click.option('--travel-plan', help='name of the travel plan configuration')
 @click.pass_context
-def travel_back(ctx):
+def travel_back(ctx, travel_plan):
     """
     Sets DB state back to the recent checkpoint 
     """
-    session_data = load_session_data()
-    ctx.invoke(travel_to, checkpoint=session_data['recent_checkpoint'])
+    session_data = load_session_data(travel_plan)
+    ctx.invoke(travel_to, travel_plan=travel_plan, checkpoint=session_data['recent_checkpoint'])
 
 
 @cli.command()
-def list():
+@click.option('--travel-plan', help='name of the travel plan configuration')
+def list(travel_plan):
     """
     Lists all checkpoints
     """
+    config = load_configuration()
+    data_share = config[travel_plan]['data_share']
+
     # TODO mark current checkpoint
-    repo = git.repo.base.Repo(path=host_data_directory)
+    repo = git.repo.base.Repo(path=data_share)
     [print(tag) for tag in repo.tags]
+
+@cli.command()
+@click.option('--travel-plan', help='name of the travel plan configuration')
+def stop(travel_plan):
+    session_data = load_session_data(travel_plan)
+    docker_client = create_docker_client()
+    container_id = session_data['container_id']
+    docker_client.stop(container_id)
+    ok('stopped container "{}"'.format(container_id))
+
 
 
 def main():
