a659264463dd2442efcd55afab824f1ea8a0a6a9
Benjamin Friedrich <benjamin.friedrich@zalando.de>
2015-02-01 09:27:49 +0100

    implemented 'tardis save <checkpoint>'

--- tardis/cli.py
+++ tardis/cli.py
@@ -1,10 +1,43 @@
 import click
 from docker import Client
 from docker.utils import kwargs_from_env
+import json
+import git
+from tardis.utils import ok, error, warn
+
+
+POSTGRES_DATA_MOUNT = '/var/lib/postgresql/data'
+POSTGRES_USER = 'postgres'
+POSTGRES_PASSWORD= 'postgres'
+
+session_file = '.tardis.session'
+
+docker_image = 'postgres:9.4.0'
+host_data_directory = '/tmp/postgres'
+docker_port = 5432
+
 
 @click.group()
 def cli():
     pass
+
+
+def dump_to_session_data(data):
+    with open(session_file, 'w') as f:
+        json.dump(data, f)
+
+ 
+def load_session_data():
+  with open(session_file, 'r') as f:
+        return json.load(f)
+
+
+def create_docker_client():
+    # c = Client(base_url='unix://var/run/docker.sock')
+    # worakaround for boot2docker
+    kwargs = kwargs_from_env()
+    kwargs['tls'].assert_hostname = False
+    return Client(**kwargs)
 
 @cli.command()
 @click.option('--docker_image', help='path to pull postgres docker image')
@@ -22,6 +55,7 @@ def configure(docker_image):
     print('configure')
 
 
+# TODO error handling
 @cli.command()
 @click.option('--config_path', help='path to tardis config file', default='.')
 def run(config_path):
@@ -29,25 +63,14 @@ def run(config_path):
     Runs your local DB image according to tardis configuration
     """
     
-    docker_image = 'postgres:9.4.0'
-    host_data_directory = '/tmp/postgres'
-    docker_port = 5432
-
-    POSTGRES_DATA_MOUNT = '/var/lib/postgresql/data'
-    POSTGRES_USER = 'postgres'
-    POSTGRES_PASSWORD= 'postgres'
-
-    # c = Client(base_url='unix://var/run/docker.sock')
-    # worakaround for boot2docker
-    kwargs = kwargs_from_env()
-    kwargs['tls'].assert_hostname = False
-    client = Client(**kwargs)
+    client = create_docker_client();
 
     container = client.create_container(docker_image, environment = { 'POSTGRES_USER': POSTGRES_USER, 
                                                                       'POSTGRES_PASSWORD': POSTGRES_PASSWORD })
-
     container_id = container.get('Id')
-    click.echo('created container for image {}. continer id is {}'.format(docker_image, container_id))
+    dump_to_session_data({ 'container_id' : container_id })
+
+    ok('created container for image {}. continer id is {}'.format(docker_image, container_id))
 
     response = client.start(container = container_id, 
                             binds = { 
@@ -58,8 +81,15 @@ def run(config_path):
                                         }
                                     },
                             port_bindings = { 5432: docker_port } )
-    click.echo('container is started')
+    ok('container is started')
 
+
+def is_dirty():
+    """
+    There is a is_dirty function in git-python but it seems that it is does not work properly
+    """
+    repo = git.repo.base.Repo(path=host_data_directory)
+    return len(repo.untracked_files) != 0
 
 
 @cli.command()
@@ -68,7 +98,27 @@ def save(checkpoint):
     """
     Sets a checkpoint for the current DB state
     """
-    print('save ' + checkpoint)
+    session_data = load_session_data()
+    docker_client = create_docker_client();
+    container_id = session_data['container_id']
+    
+    git_cmd = git.Git(host_data_directory)
+    
+    if is_dirty():      
+        try:
+            docker_client.pause(container_id)
+            ok('paused container {}'.format(container_id))
+            click.echo('repo has changed...')
+            git_cmd.add('--all', host_data_directory)
+            git_cmd.commit(message=checkpoint)
+            git_cmd.tag('--annotate', checkpoint, message=checkpoint)
+        except Exception as e:
+            error(e)
+        finally:
+            docker_client.pause(container_id)
+            ok("unpaused container {}".format(container_id))
+    else:
+        warn('repo has not changed... -> no checkpoint was created')
 
 
 @cli.command('travel-to')
@@ -97,7 +147,17 @@ def list():
 
 
 def main():
+    #pid = str(os.getpid())
+    # session_file = '.tardis.session.{}.pid'.format(pid)
+    
+    #with open(session_file, 'w') as f:
+    #    f.write('start')
+    #try:
     cli()
+    #except e:
+    #    print(e)
+    #    os.unlink(session_file)
+
 
 
 if __name__ == '__main__':
